fcf5ac8a1316dff7dd00855cec3e482d77c95b4d
Alex Bonstrom <alexbonstrom@gmail.com>
2016-09-29 19:55:01 -0400

    Done. Need to debug
    Swag

--- main.c
+++ main.c
@@ -8,9 +8,9 @@ Team Members:
 #include <stdlib.h>
 #include <assert.h>
 
-void swap(char x, char y);
-void initKey(FILE fileRead, char *keyArray);
+void swap(char *x, char *y);
 char generateKeyByte(char s[]);
+void encryptData(FILE *input, FILE *output);
 
 char S[256];
 char T[256];
@@ -26,8 +26,8 @@ int main(int argc, char *argv[]) {
    //if (argc < 3) printf("Input, output, and key files need to be specified\n");
         
     keyFile = fopen(argv[1], "r");
-    //inputFile = fopen(argv[2], "r");
-    //outputFile = fopen(argv[3], "w");
+    inputFile = fopen(argv[2], "r");
+    outputFile = fopen(argv[3], "w");
     
     int kLength = 0;
     int inputChar;
@@ -49,20 +49,20 @@ int main(int argc, char *argv[]) {
     
     for (i = 0; i < 256; i++) {
         j = (j + S[i] + T[i]) % 256;
-        swap(S[i], S[j]);
+        swap(&S[i], &S[j]);
     }
+    
+    encryptData(inputFile, outputFile);
     
     fclose(keyFile);
     fclose(inputFile);
     fclose(outputFile);
-    for(i=0; i<256; i++)
-    printf("%c\t %c\n", S[i], T[i]);
 }
 
-void swap(char x, char y) {
-    char temp = x;
-    x = y;
-    y = temp;
+void swap(char *x, char *y) {
+    char temp = *x;
+    *x = *y;
+    *y = temp;
 }
 
 char generateKeyByte(char s[]) {
@@ -72,7 +72,18 @@ char generateKeyByte(char s[]) {
     
     i = (i+1) % 256;
     j = (j+S[i]) % 256;
-    swap(S[i], S[j]);
+    swap(&S[i], &S[j]);
     int t = (S[i] + S[j]) % 256;
     return keyByte = S[t];
+}
+
+void encryptData(FILE *input, FILE *output){
+    char inChar;
+    char outChar;
+    char keyByte;
+    while ((inChar = fgetc(input)) != EOF) {
+        keyByte = generateKeyByte(S);
+        outChar = keyByte ^ inChar;
+        fputc(outChar, output);
+    } 
 }
