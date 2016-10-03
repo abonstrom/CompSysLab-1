5a59527e79983b34d29d6b4a6e79c0a0bb8ee7f9
Alex Smith <a.r.smith603@gmail.com>
2016-10-03 14:51:46 -0400

    further documentation

--- encryption.c
+++ encryption.c
@@ -8,7 +8,8 @@ Team Members:
 #include <stdlib.h>
 #include <assert.h>
 
-void swap(char *x, char *y);
+//Initialize Functions
+void swap(char *x, char *y); 
 char generateKeyByte(char s[]);
 void encryptData(FILE *input, FILE *output);
 
@@ -23,7 +24,7 @@ int main(int argc, char *argv[]) {
     FILE *outputFile;
    
     //assertion to make sure that there are enough inputs on the command line
-    assert (argc == 3);
+    assert (argc == 4);
     
     keyFile = fopen(argv[1], "r");
     inputFile = fopen(argv[2], "r");
@@ -32,25 +33,24 @@ int main(int argc, char *argv[]) {
     int kLength = 0;
     int inputChar;
     //checks next char in keyFile and if it is not EOF or over 256 then add 
-    //one to length 
+    //one to length and insert char into key[] 
     while((inputChar = fgetc(keyFile)) != EOF && kLength<256){
         key[kLength] = inputChar;
         kLength++;
     }   
-    
+    //Loop through and fill T[] fully with key[] even if key[] is not completely
+    //full
     int i = 0;
-    
     while (i < 256){
-        S[i] = i;
+        S[i] = i; //adds placeholder for elements in S[]
         T[i] = key[i % kLength];
         i++;
     }
-
-    int j = 0;
     
-    for (i = 0; i < 256; i++) {
-        j = (j + S[i] + T[i]) % 256;
-        swap(&S[i], &S[j]);
+    int j = 0;
+    for (i = 0; i < 256; i++) {  
+        j = (j + S[i] + T[i]) % 256;//Scrambles value of full key T[] and inputs
+        swap(&S[i], &S[j]); //adds extra layer of complexity
     }
     
     encryptData(inputFile, outputFile);
@@ -61,20 +61,20 @@ int main(int argc, char *argv[]) {
     fclose(outputFile);
 }
 
-void swap(char *x, char *y) {
+void swap(char *x, char *y) { //swaps values at end of pointers IE. array values
     char temp = *x;
     *x = *y;
     *y = temp;
 }
 
-char generateKeyByte(char s[]) {
-    static int i=0;
+char generateKeyByte(char s[]) {//Generates byte to be xor with output char
+    static int i=0;//static int i,j hold value even after initial return.
     static int j=0;
     char keyByte;
     
     i = (i+1) % 256;
-    j = (j+S[i]) % 256;
-    swap(&S[i], &S[j]);
+    j = (j+S[i]) % 256; //uses S[] which is mixed T[] which is mixed key[]
+    swap(&S[i], &S[j]);//swap pointer to S[i] and S[j]
     int t = (S[i] + S[j]) % 256;
     return keyByte = S[t];
 }
@@ -83,7 +83,7 @@ char generateKeyByte(char s[]) {
 void encryptData(FILE *input, FILE *output){
     char inChar;
     char outChar;
-    //variable that will hold the next value in the keyStream, will be ex-ored 
+    //variable that will hold the next value in the keyStream, will be xored 
     //with the input character to create the encrypted character
     char keyByte;
     while ((inChar = fgetc(input)) != EOF) {
